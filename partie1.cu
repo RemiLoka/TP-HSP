e9f02d483a9c712df01a8fd44fbad1639558f695
aminedahire <amine.dahire@ensea.fr>
2023-11-27 14:11:35 +0100

    update

--- partie1.cu
+++ partie1.cu
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Initialisation de la matrice
+
 void MatrixInit(float *M, int n, int p) {
     for (int i = 0; i < n * p; i++) {
         M[i] = ((float)rand() / (float)(RAND_MAX)) * 2.0 - 1.0;
@@ -20,5 +20,29 @@ void MatrixPrint(float *M, int n, int p) {
 
 
 void MatrixAdd(float *M1, float *M2, float *Mout, int n, int p){
-    
+    for (int i = 0; i < n * p; i++) {
+        Mout[i] = M1[i] + M2[i];
+    }
 }
+
+void MatrixMult(float *M1, float *M2, float *Mout, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++){
+            float sum = 0;
+            for (int k = 0; k < n; k++) {
+                sum += M1[i * n + k] * M2[k * n + j];
+            }
+            Mout[i * n + j] = sum;
+        }
+    }
+}
+
+
+__global__ void cudaMatrixAdd(float *M1, float *M2, float *Mout, int n, int p){
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i < n && j < p) {
+        Mout[i * p + j] = M1[i * p + j] + M2[i * p + j];
+    }
+}
+
